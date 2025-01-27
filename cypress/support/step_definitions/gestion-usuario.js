dd2961b4e3a85d5c0f461d21a665931db3c93983
QA_Solverpay <yamileidi.gonzalez@f1-connecting.com>
2025-01-27 15:55:58 +0100

    Actualizacion 15

---
+++ cypress/support/step_definitions/gestion-usuario.js
@@ -0,0 +1,16 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given("que el usuario está en la página de login", () => {
+  cy.visit("https://newfront.lab.solverpay.com/login"); // Reemplaza con la URL real
+});
+
+When("ingresa un nombre de usuario {string} y una contraseña {string}", (username, password) => {
+  cy.get('#user').type(username); // Reemplaza con el selector real
+  cy.get('#password').type(password);
+});
+
+Then("presiona el botón de login", () => {
+    cy.get('.mt-2').click(); // Reemplaza con el selector real del botón
+});
+
+
