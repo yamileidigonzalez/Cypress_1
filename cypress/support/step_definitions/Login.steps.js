37900d65e4ca4a4b3aeea2464b819639bf7b2f02
QA_Solverpay <yamileidi.gonzalez@f1-connecting.com>
2025-01-28 13:53:01 +0100

    Actualizacion 18

--- cypress/support/step_definitions/Login.steps.js
+++ cypress/support/step_definitions/Login.steps.js
@@ -4,19 +4,23 @@ import { Given as GivenLogin } from './Common_steps';
 GivenLogin();
 
 When('ingresa un nombre de usuario {string} y una contraseña {string}', (username, password) => {
+  console.log("He llegado aquí")
   cy.get('#user').type(username); // Reemplaza con el selector real
+  console.log("He llegado aquí")
   cy.get('#password').type(password);
+  console.log("He llegado aquí")
 });
-/*
+
 When('no ingresa un nombre de usuario ni contraseña', () => {
   cy.get('#user').clear(); // Asegúrate de que los campos estén vacíos
   cy.get('#password').clear();
-});*/
+});
 
 Then('presiona el botón de login', () => {
+  console.log("He llegado aquí")
   cy.get('.mt-2').click(); // Cambia el selector si es necesario
 });
-/*
+
 Then('el usuario debería ser redirigido al dashboard', () => {
   cy.url().should('include', '/dashboard'); // Cambia '/dashboard' por la ruta del dashboard
 });
@@ -30,6 +34,6 @@ Then('el usuario debería ver un mensaje de advertencia', () => {
     .should('be.visible')
     .and('contain', 'Por favor, complete todos los campos'); // Ajusta el mensaje según tu app
 });
-*/
+
 
 
