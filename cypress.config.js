18223acfeb7a9e5e2874454a5042fdac882be604
QA_Solverpay <yamileidi.gonzalez@f1-connecting.com>
2025-01-28 16:04:19 +0100

    Actualizacion 120

--- cypress.config.js
+++ cypress.config.js
@@ -11,20 +11,25 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       const bundler = createBundler({
-        plugins: [createEsbuildPlugin(config)],
+        plugins: [createEsbuildPlugin(config)],// Agregar el plugin de esbuild
+        platform: 'node', // Configurar esbuild para usar el entorno de Node.js
       });
       on("file:preprocessor", bundler);
       addCucumberPreprocessorPlugin(on, config);
       return config;
     },
     specPattern: "cypress/e2e/**/*.feature", // Busca solo archivos .feature en la carpeta e2e
+    stepDefinitions: [
+      "cypress/e2e/**/*.steps.js",
+      "cypress/support/step_definitions/*.steps.js",
+    ],
     //stepDefinitions: "cypress/support/step_definitions/*steps.js", // Ruta a los archivos de pasos
-    stepDefinitions: (spec) => {
+    /*stepDefinitions: (spec) => {
       if (spec.includes('Login.feature')) {
         return 'cypress/support/step_definitions/Login.steps.js'; // Ruta al archivo con las definiciones
       }
       return 'cypress/support/step_definitions/*.{js,ts}';
-    },
+    },*/
     experimentalStudio: true
   }
 });
